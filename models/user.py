0fed06c288759b363fa3945ec21039e283794ed7
Cynthia Taylor <820@holbertonschool.com>
2020-06-30 19:53:08 +0000

    Add user class

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" User Class """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class inherits from BaseModel class """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
